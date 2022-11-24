7b1d5b87110027c6cedbe7089e4c77b4119d2396
Shuyu (Joshua) Zhou <35708194+szhou12@users.noreply.github.com>
2022-11-23 20:09:05 -0600

    add updates

--- src/node2.py
+++ src/node2.py
@@ -4,6 +4,10 @@ import time
 import socket
 import random
 import logging
+from timer import ResettableTimer
+import dataclasses
+from dataclasses import dataclass
+from enum import Enum
 
 from .log import Log
 from .config import config
@@ -12,9 +16,33 @@ logger = logging.getLogger(__name__)
 logger.setLevel(level=logging.INFO)
 logger.propagate = False
 
+class Role(Enum):
+    Follower = "Follower"
+    Leader = "Leader"
+    Candidate = "Candidate"
+
+@dataclass
+class PersistentState:
+    pass
+
+
+@dataclass
+class LogEntry:
+    message: str
+    term: int
+
+
+@dataclass
+class VoteRequest:
+    term: int
+    candidate_id: int
+    last_log_index: int
+    last_log_term: int
+
+    
 class Node(object):
     def __init__(self, metadata: dict):
-        self.role = 'follower'
+        self.role = Role.Follower
 
         self.group_id = metadata['group_id']
         # current raft peer's index in peers ????
@@ -80,5 +108,68 @@ class Node(object):
         conf = config[env] if env else config['DEV']
         return conf
     
+    def load(self):
+        filename = os.path.join(self.path, f'{self.group_id}_{self.id}_persist.json')
+        if os.path.exists(filename):
+            with open(filename, 'r') as f:
+                persist_data = json.load(f)
+            self.current_term = persist_data['current_term']
+            self.voted_for = persist_data['voted_for']
+        else:
+            self.save()
+
+    def save(self):
+        filename = os.path.join(self.path, f'{self.group_id}_{self.id}_persist.json')
+        persist_data = {
+            'current_term': self.current_term,
+            'voted_for': self.voted_for
+        }
+        with open(filename, 'w') as f:
+            json.dump(persist_data, f, indent=4)
+
+    def redirect(self, data, addr):
+        '''
+        Redirect to Leader node
+        Args:
+            data: dict
+            addr: tuple(ip, port)
+        Return:
+            dict // Change it to response {'success': bool, 'data': dict}
+        '''
+        if not data:
+            return {} # TODO
+        
+        if data.get('type') == 'client_append_entries':
+            if self.role != Role.Leader:
+                if self.leader_id:
+                    logger.info(f"redirect client_append_entries to leader node: {self.leader_id}")
+                    self.rpc_endpoint.send(data, self.peers.get(self.leader_id)) # TODO: change later
+                return {} # TODO
+            else: # me is leader node
+                self.client_addr = (addr[0], self.conf.cport)
+                return data # TODO
+        
+        if data.get('dst_id') != self.id:
+            logger.info(f"redirect to: {data.get('dst_id')}")
+            self.rpc_endpoint.send(data, self.peers.get(data.get('dst_id')))
+            return {} # TODO
+        
+        return data
     
+    def append_entries():
+        '''
+        Append Entries RPC
+        Invoked by leader to replicate log entries; also used as heartbeat
+        '''
+        # TODO
+        pass
+
+    def request_vote():
+        '''
+        RequestVote RPC
+        Invoked by candidates to gather votes
+        '''
+
+    
+
 
